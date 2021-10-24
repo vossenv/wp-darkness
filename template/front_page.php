139ac1c1650baccb63a3c2f78e5273f4f31c4eb6
vossenv <vossen.dm@gmail.com>
2021-10-24 00:45:05 -0500

    current

--- template/front_page.php
+++ template/front_page.php
@@ -15,40 +15,24 @@ get_header(); ?>
     <div id="primary" class="front_page featured-content content-area">
         <main id="main" class="site-main">
 
-            <div id="title"><b>ALL THE PRETTY HORSES</b></div>
+            <div id="title-text"><b>Venus DeMars<br>&<br>All The Pretty Horses</b></div>
 
-
-            <div id="arrow-container">
-                <div id="arrow-pulse"></div>
-                <div id="arrow-inner">
-                    <a href="#tp-slider-home">
-                        <img id="arrow-img" src=<?php echo get_theme_file_uri("img/arrow.svg") ?>>
-                    </a>
-                </div>
+            <div id="title-img">
+                <?php
+                $img_f = get_theme_file_uri('/img/ATPHcircle-logo.gif');
+                echo '
+                    <img id="front-page-image" src="' . $img_f . '">
+                    '
+                ?>
             </div>
-
-<!--            <div id="news">-->
-<!--                --><?php //echo do_shortcode('[masterslider alias="ms-2"]'); ?>
-<!--            </div>-->
-<!--            <div id="news-mobile">-->
-<!--                --><?php //echo do_shortcode('[masterslider alias="ms-2-2"]'); ?>
-<!--            </div>-->
 
             <div id="tp-slider-home"></div>
-            <div id="triggeredpaintz" class="trans_row">
-                <span class="section-title">Band</span>
-                <hr>
-                <h3>ATPH</h3>
-            </div>
-<!--            <div id="verein" class="trans_row">-->
-<!--                <span class="section-title">TP.tz Verein</span><br/>-->
-<!--                <hr>-->
-<!--            </div>-->
 
             <div id="paintball" class="trans_row">
                 <span class="section-title">XXXXX</span>
                 <hr>
             </div>
+
             <div id="soziales" class="trans_row">
                 <span class="section-title">Social</span>
                 <hr>
@@ -69,40 +53,40 @@ get_header(); ?>
                 <hr>
             </div>
             <div id="kontakt" class="trans_row">
-                <span class="section-title">Kontakt</span>
+                <span class="section-title">Contact</span>
                 <hr>
                 <br/>
-                <p>Haben wir dein Interesse an unserem Team geweckt? Wir freuen uns auf deine Nachricht!</p>
+                <p>Contact us</p>
                 <?php echo do_shortcode('[wpforms id="281"]'); ?>
             </div>
-<!--            <div id="impressum" class="trans_row">-->
-<!--                <span class="section-title">Impressum</span>-->
-<!--                <hr>-->
-<!--                <div class="impressum-text">-->
-<!--                    <table id="impressum-table">-->
-<!--                        <tr>-->
-<!--                            <td class='field_name'>Rechtsform:</td>-->
-<!--                            <td>Eingetragener Verein</td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td class='field_name'>Adresse:</td>-->
-<!--                            <td>TriggeredPain.tz e.V</td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td class='field_name'>Telefon:</td>-->
-<!--                            <td>12345868</td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td class='field_name'>E-Mail:</td>-->
-<!--                            <td><a href="mailto:someone@yoursite.com">vorstand@triggeredpaintz.com</a></td>-->
-<!--                        </tr>-->
-<!--                        <tr>-->
-<!--                            <td class='field_name'>Register:</td>-->
-<!--                            <td>E127491234798</td>-->
-<!--                        </tr>-->
-<!--                    </table>-->
-<!--                </div>-->
-<!--            </div>-->
+            <!--            <div id="impressum" class="trans_row">-->
+            <!--                <span class="section-title">Impressum</span>-->
+            <!--                <hr>-->
+            <!--                <div class="impressum-text">-->
+            <!--                    <table id="impressum-table">-->
+            <!--                        <tr>-->
+            <!--                            <td class='field_name'>Rechtsform:</td>-->
+            <!--                            <td>Eingetragener Verein</td>-->
+            <!--                        </tr>-->
+            <!--                        <tr>-->
+            <!--                            <td class='field_name'>Adresse:</td>-->
+            <!--                            <td>TriggeredPain.tz e.V</td>-->
+            <!--                        </tr>-->
+            <!--                        <tr>-->
+            <!--                            <td class='field_name'>Telefon:</td>-->
+            <!--                            <td>12345868</td>-->
+            <!--                        </tr>-->
+            <!--                        <tr>-->
+            <!--                            <td class='field_name'>E-Mail:</td>-->
+            <!--                            <td><a href="mailto:someone@yoursite.com">vorstand@triggeredpaintz.com</a></td>-->
+            <!--                        </tr>-->
+            <!--                        <tr>-->
+            <!--                            <td class='field_name'>Register:</td>-->
+            <!--                            <td>E127491234798</td>-->
+            <!--                        </tr>-->
+            <!--                    </table>-->
+            <!--                </div>-->
+            <!--            </div>-->
 
             <?php get_template_part('template-parts/content', 'single'); ?>
 
