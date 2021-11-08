6fa621c87baaed2658b98dc0ff28f0a8b0382217
vossenv <vossen.dm@gmail.com>
2021-11-08 01:33:51 -0600

    good

--- header.php
+++ header.php
@@ -24,46 +24,39 @@
 <a class="skip-link screen-reader-text" href="#primary"><?php _e('Skip to content', 'blogrid'); ?></a>
 
 <div id="page" class="site">
-    <?php if (is_front_page()) : ?>
-
-        <!-- Header img -->
-        <?php if (get_header_image()) : ?>
-            <div class="header-div" style="background-image:url(<?php echo esc_url((get_header_image())); ?>)">
-
-                <div class="dark-logo-container">
-                    <?php if (has_custom_logo()) : ?>
-                        <?php the_custom_logo(); ?>
-                    <?php endif; ?>
-                </div>
-
-                <div class="header-title-container">
-                    <div id="h-title-text"><b>Venus DeMars<br>&<br>All The Pretty Horses</b></div>
-                </div>
-
-<!--                <img src="--><?php //echo esc_url((get_header_image())); ?><!--"-->
-<!--                     alt="--><?php //echo esc_attr((get_bloginfo('title'))); ?><!--"/>-->
-            </div>
-        <?php endif; ?>
-        <!-- / Header img -->
-
-    <?php endif; ?>
-
     <header id="masthead" class="sheader site-header clearfix">
-        <nav id="primary-site-navigation" class="primary-menu main-navigation clearfix">
+    <div class="header-inner-mobile">
+        <div class="dark-logo-container">
+            <?php if (has_custom_logo()) : ?>
+                <?php the_custom_logo(); ?>
+            <?php endif; ?>
+        </div>
 
+        <div class="header-title-container">
+            <div id="h-title-text"><b>Venus DeMars & All The Pretty Horses</b></div>
+        </div>
+        <div class="ham-menu">
+            <a href="#" id="pull" class="toggle-mobile-menu menu-toggle" aria-controls="secondary-menu"
+               aria-expanded="false"></a>
+        </div>
+    </div>
+
+    <div class="header-div" style="background-image:url(<?php echo esc_url((get_header_image())); ?>)">
+        <div class="header-contents">
+        <!--        <div class="dark-logo-container">-->
+        <!--            --><?php //if (has_custom_logo()) : ?>
+        <!--                --><?php //the_custom_logo(); ?>
+        <!--            --><?php //endif; ?>
+        <!--        </div>-->
+
+        <div class="header-title-container">
+            <div id="h-title-text"><b>Venus DeMars<br>&<br>All The Pretty Horses</b></div>
+        </div>
+        <nav id="primary-site-navigation" class="primary-menu main-navigation clearfix">
             <a href="#" id="pull" class="smenu-hide toggle-mobile-menu menu-toggle" aria-controls="secondary-menu"
                aria-expanded="false"><?php esc_html_e('Menu', 'blogrid'); ?></a>
             <div class="top-nav-wrapper">
                 <div class="menu-content-wrap">
-<!--                    <div class="logo-container">-->
-<!---->
-<!--                        --><?php //if (has_custom_logo()) : ?>
-<!--                            --><?php //the_custom_logo(); ?>
-<!--                        --><?php //else : ?>
-<!--                            <a class="logofont" href="--><?php //echo esc_url(home_url('/')); ?><!--"-->
-<!--                               rel="home">--><?php //bloginfo('name'); ?><!--</a>-->
-<!--                        --><?php //endif; ?>
-<!--                    </div>-->
                     <div class="center-main-menu">
                         <?php
                         wp_nav_menu(array(
@@ -76,22 +69,8 @@
                 </div>
             </div>
         </nav>
-
-        <div class="super-menu clearfix">
-            <div class="super-menu-inner">
-
-
-                <?php if (has_custom_logo()) : ?>
-                    <?php the_custom_logo(); ?>
-                <?php else : ?>
-                    <a class="logofont" href="<?php echo esc_url(home_url('/')); ?>"
-                       rel="home"><?php bloginfo('name'); ?></a>
-                <?php endif; ?>
-                <a href="#" id="pull" class="toggle-mobile-menu menu-toggle" aria-controls="secondary-menu"
-                   aria-expanded="false"></a>
-            </div>
         </div>
-        <div id="mobile-menu-overlay"></div>
+
     </header>
 
     <div class="content-wrap">
